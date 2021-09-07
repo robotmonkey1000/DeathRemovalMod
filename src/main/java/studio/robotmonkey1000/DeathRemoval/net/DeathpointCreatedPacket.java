905c121803fe770ac7d843246b0b33274f756681
robotmonkey1000 <robotmonkey000@gmail.com>
2021-09-07 17:18:44 -0400

    Version Update

--- src/main/java/studio/robotmonkey1000/DeathRemoval/net/DeathpointCreatedPacket.java
+++ src/main/java/studio/robotmonkey1000/DeathRemoval/net/DeathpointCreatedPacket.java
@@ -2,6 +2,7 @@ package studio.robotmonkey1000.DeathRemoval.net;
 
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkEvent;
 import studio.robotmonkey1000.DeathRemoval.Deathremoval;
 
@@ -33,9 +34,27 @@ public class DeathpointCreatedPacket {
         if (context.getDirection().getReceptionSide() == LogicalSide.SERVER) {
             Deathremoval.LogMessage(ctx.get().getSender().getName().getString() + " has created a death waypoint!");
             ctx.get().enqueueWork(() -> {
+                //The player has a corpse created when they died.
                 if( Deathremoval.recentDeathForPlayer.containsKey(context.getSender().getUniqueID())) {
-                    Deathremoval.recentDeathForPlayer.get(ctx.get().getSender().getUniqueID()).setNoGravity(true);
-                    Deathremoval.recentDeathForPlayer.get(ctx.get().getSender().getUniqueID()).setPosition(msg.x, msg.y, msg.z);
+
+                    //The corpse is not null
+                    if(Deathremoval.recentDeathForPlayer.get(context.getSender().getUniqueID()) != null) {
+
+                        //The corpse still exists within the world
+                        if(Deathremoval.recentDeathForPlayer.get(ctx.get().getSender().getUniqueID()).isAlive()) {
+                            Deathremoval.LogMessage("Corpse Exists!");
+                            Deathremoval.recentDeathForPlayer.get(ctx.get().getSender().getUniqueID()).setNoGravity(true);
+                            Deathremoval.recentDeathForPlayer.get(ctx.get().getSender().getUniqueID()).setPosition(msg.x, msg.y, msg.z);
+                        } else /*The corpse has been removed*/ {
+                            Deathremoval.LogMessage("Players Corpse Has Despawned before waypoint creation! Removing Waypoint.");
+
+                            //Send packet to remove this waypoint as it is not needed anymore.
+                            DeathRemovalPacketHandler.INSTANCE.sendTo(new RemoveWayPointPacket(msg.x, msg.y, msg.z), context.getSender().connection.getNetworkManager(), NetworkDirection.PLAY_TO_CLIENT);
+
+                        }
+                    }
+                } else {
+                    Deathremoval.LogMessage("Player has no corpse!");
                 }
             });
             ctx.get().setPacketHandled(true);
